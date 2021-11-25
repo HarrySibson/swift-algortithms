4bf44236bb7a84fe4af9d3f891fabc2a02938b01
HarrySibson <94445782+HarrySibson@users.noreply.github.com>
2021-11-25 13:03:26 +0000

    Update SearchingTest.swift

--- StandardAlgorithmsTests/SearchingTest.swift
+++ StandardAlgorithmsTests/SearchingTest.swift
@@ -8,6 +8,16 @@
 import XCTest
 
 class SearchingTest: XCTestCase {
+  func testingLinearSearchAlgorithmExpectingToFindValues(){
+    let searching = Searching()
+    testCases = ((searchArray: [1,2,3,4,5,6,7,8,9,10], find: 5), (searchArray: [12,23,34,45,56,67,78,89,90], find:56))
+    for testCase in testCases{
+      actual = searching.linearSearch(testCase.searchArray,testCase.find)
+      XCTAssertEqual(actual,True)
+    }
+  
+  }
+
 
 
 }
