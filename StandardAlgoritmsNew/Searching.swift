aa8df54340c4fdb35db62020d5373a274ad3a4d9
HarrySibson <94445782+HarrySibson@users.noreply.github.com>
2021-11-29 18:16:54 +0000

    Update Searching.swift

--- StandardAlgoritmsNew/Searching.swift
+++ StandardAlgoritmsNew/Searching.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 class Searching{
-    
+    func linearSearch(_:anyArray,_:search)->bool{
+        var found = 0
+        var anyArray = anyArray
+        let search = search
+        for i in 0..<anyArray.count{
+            if anyArray[i] == search{
+            found = found + 1 
+            }
+        }
+        if found != 0{
+            return True
+        }
+    }
 }
